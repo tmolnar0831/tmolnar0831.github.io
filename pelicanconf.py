b475483b315dea8f815b52f7e55ed344f8e89c0b
Tamas Molnar <tmolnar0831@gmail.com>
2022-04-28 19:06:33 +0200

    Initialize Pelican Blog Setup

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Tamas Molnar'
+SITENAME = "Tom's IT Cafe"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Budapest'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'))
+
+# Social widget
+SOCIAL = (('Reddit', 'https://www.reddit.com/user/st_iron'),
+          ('YouTube', 'https://www.youtube.com/channel/UC28ChJtG8OTp1_5AWwpoh0w'),
+          ('GitHub', 'https://github.com/tmolnar0831'))
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
